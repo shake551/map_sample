63d7793f16588eb848dab46c7478269a1aa00ef5
shake551 <hiroki.yamako@ezweb.ne.jp>
2022-09-22 00:51:51 +0900

    add pin

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,87 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
+class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+class MapSampleState extends State<MapSample> {
+  Position? currentPosition;
+  late GoogleMapController _controller;
+  late StreamSubscription<Position> positionStream;
+  //初期位置
+  final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(43.0686606, 141.3485613),
+    zoom: 14,
+  );
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if(permission == LocationPermission.denied){
+        await Geolocator.requestPermission();
+      }
+    });
+
+    //現在位置を更新し続ける
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+          currentPosition = position;
+          print(position == null
+              ? 'Unknown'
+              : '${position.latitude.toString()}, ${position.longitude.toString()}');
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      myLocationEnabled: true,//現在位置をマップ上に表示
+      markers: _createMarker(),
+      onMapCreated: (GoogleMapController controller) {
+        _controller = controller;
+      },
     );
   }
+}
+
+Set<Marker> _createMarker() {
+  return {
+    Marker(
+      markerId: MarkerId("marker_1"),
+      position: LatLng(43.0686606, 141.3485613),
+    ),
+  };
 }
